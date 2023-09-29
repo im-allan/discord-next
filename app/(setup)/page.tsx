dc5bdd7ebbc86f808c7ae3111060141590800f9d
im-allan <al.silvaquiroz@gmail.com>
2023-09-29 15:45:42 -0300

    feat: Added modal component on main page

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,3 +1,4 @@
+import { InitialModal } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -18,7 +19,7 @@ const SetupPage = async() => {
   if (server) {
     return redirect(`/servers/${server.id}`)
   }
-  return <section>Create A Server</section>;
+  return <InitialModal/>;
 }
  
 export default SetupPage;
