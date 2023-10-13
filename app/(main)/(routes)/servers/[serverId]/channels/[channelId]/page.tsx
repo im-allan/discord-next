d4cb3168d1d0ae3e5d58ff9d146e3c2998bdeedb
im-allan <al.silvaquiroz@gmail.com>
2023-10-13 15:17:17 -0300

    feat: Added conversation and channel page

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,8 +1,46 @@
-const ChannelPage = () => {
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { ChatHeader } from "@/components/chat/chat-header";
+
+
+interface ChannelPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  }
+}
+
+const ChannelPage = async({ params }: ChannelPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    }
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+
   return ( 
-    <div>
-      Channel ID Page
-    </div>
+    <section className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader name={channel.name}  serverId={channel.serverId} type="channel" />
+    </section>
    );
 }
  
