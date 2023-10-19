0430812d9ff35d732136018fad14f66cb947cd63
im-allan <al.silvaquiroz@gmail.com>
2023-10-18 23:07:39 -0300

    feat: Added chat header component

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,8 @@
 import { Hash } from "lucide-react";
 import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
+import { SocketIndicator } from "@/components/socket-indicator";
+import { ChatVideoButton } from "@/components/chat/chat-video-button";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -20,6 +22,12 @@ export const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) 
         <UserAvatar src={ imageUrl } className="h-5 w-5 md:h-5 md:w-5 mr-2"/>
       )}
       <p className="font-semibold text-md text-black dark:text-white"> {name} </p>
+      <div className="ml-auto flex items-center">
+        {type === "conversation" && (
+          <ChatVideoButton />
+        )}
+        <SocketIndicator />
+      </div>
     </section>
   )
 }
